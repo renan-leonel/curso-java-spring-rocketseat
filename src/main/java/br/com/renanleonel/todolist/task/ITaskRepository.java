aa5b2817d3ac6b04432ac1bfa0537126f220eae4
renan-leonel <renanleonelpro@gmail.com>
2023-10-12 14:45:22 -0300

    feat: aula 4

--- src/main/java/br/com/renanleonel/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/renanleonel/todolist/task/ITaskRepository.java
@@ -2,8 +2,9 @@ package br.com.renanleonel.todolist.task;
 
 import java.util.UUID;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-    
+    List<TaskModel> findByIdUser(UUID idUser);
 }
